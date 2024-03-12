63dcea4fcd2b57e197a0ad9d177564b231f648ee
CarlosGarciaDMarina <117413251+CarlosGarciaDMarina@users.noreply.github.com>
2024-03-12 12:37:00 +0100

    Proyecto Angular

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  public title: string;
+  public subtitle: string;
+  public email: string;
+
+  constructor() {
+    this.title = "Carlos García de Marina";
+    this.subtitle = "Desarrollador full stack especializado en el front";
+    this.email = "carlosgarciadmarina@gmail.com";
+  }
+
+  ngOnInit() {
+
+  }
+
 
 }
