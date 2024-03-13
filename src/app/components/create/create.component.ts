b1b0e34e2e6a361f27e3b025462e5ba5bd146109
CarlosGarciaDMarina <117413251+CarlosGarciaDMarina@users.noreply.github.com>
2024-03-13 12:33:01 +0100

    Formulario y configuraciones

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Project } from '../../models/projects';
+import { ProjectService } from '../../services/project.service';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrl: './create.component.css'
+  styleUrl: './create.component.css',
+  providers: [ProjectService]
 })
 export class CreateComponent {
+
+  public title: string;
+  public project: Project;
+
+
+  constructor(
+    private _projectService: ProjectService
+
+  ) {
+    this.title = "Carlos García de Marina";
+    this.project = new Project('', '', '', '', 2024, '','');
+  }
+  
+  ngOnInit() {
+
+  }
+
+  onSubmit(form:any){
+    console.log(this.project);
+  }
+
 
 }
